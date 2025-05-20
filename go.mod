0fe766e6b4130da7a85dc5a10e0810d387510ece
M. Mert Yildiran <me@mertyildiran.com>
2025-05-21 00:02:40 +0300

    Add `race` package

--- go.mod
+++ go.mod
@@ -5,10 +5,20 @@ go 1.22.2
 require (
 	github.com/getsentry/sentry-go v0.29.0
 	github.com/hashicorp/go-retryablehttp v0.7.7
+	github.com/nxadm/tail v1.4.11
+	github.com/rs/zerolog v1.33.0
+	github.com/stretchr/testify v1.8.3
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/sys v0.20.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/tomb.v1 v1.0.0-20141024135613-dd632973f1e7 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ race/race.go
@@ -0,0 +1,93 @@
+package race
+
+import (
+	"fmt"
+	"path/filepath"
+	"regexp"
+	"runtime/debug"
+	"strings"
+	"time"
+
+	"github.com/nxadm/tail"
+	"github.com/rs/zerolog/log"
+)
+
+const logSeparator string = "=================="
+const redacted string = "[REDACTED]"
+
+func WatchRaceLogs() {
+	if !isRaceFlagEnabled() {
+		return
+	}
+
+	t, err := tail.TailFile(
+		findRaceLogFile(),
+		tail.Config{
+			Follow: true,
+			ReOpen: true,
+		},
+	)
+	if err != nil {
+		log.Error().Err(err).Send()
+	}
+
+	var buffer []string
+
+	for line := range t.Lines {
+		if line.Text == logSeparator {
+			msg := strings.Join(buffer, "\n")
+			buffer = make([]string, 0)
+
+			msg = strings.TrimPrefix(msg, logSeparator)
+			msg = strings.TrimSuffix(msg, logSeparator)
+			msg = strings.TrimSpace(msg)
+			if msg != "" {
+				log.Error().Str("type", "race").Msg(msg)
+			}
+		}
+
+		buffer = append(buffer, line.Text)
+	}
+}
+
+func findRaceLogFile() string {
+	for {
+		time.Sleep(1 * time.Second)
+
+		matches, err := filepath.Glob("/tmp/kubeshark-race.log.*")
+		if err != nil {
+			log.Error().Err(err).Send()
+			continue
+		}
+
+		if len(matches) > 0 {
+			return matches[0]
+		}
+	}
+}
+
+func isRaceFlagEnabled() bool {
+	b, ok := debug.ReadBuildInfo()
+	if !ok {
+		log.Error().Err(fmt.Errorf("could not read build info")).Send()
+		return false
+	}
+
+	for _, s := range b.Settings {
+		if s.Key == "-race" && s.Value == "true" {
+			return true
+		}
+	}
+	return false
+}
+
+func redact(log string) string {
+	splitted := strings.Split(log, "\n\n")
+	log = strings.Join(splitted[:2], "\n\n")
+	return regexp.MustCompile("goroutine [0-9]+").
+		ReplaceAllString(
+			regexp.MustCompile("0[xX][0-9a-fA-F]+").
+				ReplaceAllString(log, redacted),
+			redacted,
+		)
+}
